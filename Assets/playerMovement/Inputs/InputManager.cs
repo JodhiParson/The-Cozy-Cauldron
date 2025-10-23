75762b290ce85855601e28be091529fba3f6aa70
BryceLie <brycelie02@gmail.com>
2025-10-23 17:34:37 -0400

    Added Sprinting - Increase Animation speed while Sprinting - LShift Key to Sprint

--- Assets/playerMovement/Inputs/InputManager.cs
+++ Assets/playerMovement/Inputs/InputManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public static Vector2 Movement;
+    private InputAction Sprint;
     private PlayerInput playerInput;
     private InputAction moveAction;
     [SerializeField] private PlayerMovement pMove;
@@ -12,10 +13,22 @@ public class InputManager : MonoBehaviour
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Move"];
+        Sprint = playerInput.actions["Sprint"];
     }
 
     private void Update()
     {
         Movement = moveAction.ReadValue<Vector2>();
+        bool isSprinting = Sprint != null && Sprint.IsPressed();
+
+        if (isSprinting)
+        {
+            
+            pMove.mSpeed = 20f;
+        }
+        else
+        {
+            pMove.mSpeed = 15f;
+        }
     }
 }
