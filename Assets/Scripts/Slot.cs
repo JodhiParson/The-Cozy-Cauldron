8fcfe3f960f5af415ca3356e647ed445fbd93087
BryceLie <brycelie02@gmail.com>
2025-11-04 23:01:07 -0500

    Inventory Page in Progress Modified Boss Attack

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 // using UnityEngine.
 
-public class Slot : MonoBehaviour,IDropHandler
+public class Slot : MonoBehaviour
 {
-    public void OnDrop(PointerEventData eventData)
-    {
-        if (transform.childCount == 0)
-        {
-            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
-            inventoryItem.parentAfterDrag = transform;
-        }
-    }
+    public GameObject currentItem;
 
 }
