9c71393ae73016902fce0e6cd0552289e1722918
Jodhi <jodhiparson@yahoo.com>
2025-11-10 19:23:36 -0500

    mod InventoryItem, PlayerItemCollector mod TomatoUI prefab fix bug multiple pickups for one item

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -4,73 +4,149 @@ using UnityEngine.EventSystems;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Transform originalParent;
-    CanvasGroup canvas;
-
-
+    private Transform originalParent;
+    private CanvasGroup canvasGroup;
+    private RectTransform rectTransform;
+    private Canvas rootCanvas;
 
     private void Awake()
     {
-        canvas = GetComponentInParent<CanvasGroup>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
+        rootCanvas = GetComponentInParent<Canvas>(); // top-level Canvas
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        originalParent = transform.parent;
-        transform.SetParent(transform.root);
-        canvas.blocksRaycasts = false;
-        canvas.alpha = 0.6f;
+        originalParent = rectTransform.parent;
+        rectTransform.SetParent(rootCanvas.transform); // move to top canvas so it’s on top of everything
+        canvasGroup.blocksRaycasts = false;            // allow drop detection
+        canvasGroup.alpha = 0.6f;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position = eventData.position;
+        rectTransform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvas.blocksRaycasts = true;
-        canvas.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
 
-        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+        // Check if we are dropping over a slot
+        Slot dropSlot = eventData.pointerCurrentRaycast.gameObject?.GetComponent<Slot>();
+
         if (dropSlot == null)
         {
-            GameObject item = eventData.pointerEnter;
-            if (item !=null)
-            {
-                dropSlot = item.GetComponentInParent<Slot>();
-            }
+            // If we hit a child of a slot
+            dropSlot = eventData.pointerCurrentRaycast.gameObject?.GetComponentInParent<Slot>();
         }
-        Slot originalSlot = originalParent?.GetComponent<Slot>();
+
+        Slot originalSlot = originalParent.GetComponent<Slot>();
 
         if (dropSlot != null)
         {
             if (dropSlot.currentItem != null)
             {
+                // Swap items
                 dropSlot.currentItem.transform.SetParent(originalSlot.transform);
                 originalSlot.currentItem = dropSlot.currentItem;
                 dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
             }
             else
             {
+                // Clear original slot
                 originalSlot.currentItem = null;
             }
 
-            transform.SetParent(dropSlot.transform);
+            // Put this item in the new slot
+            rectTransform.SetParent(dropSlot.transform);
             dropSlot.currentItem = gameObject;
         }
         else
         {
-            transform.SetParent(originalParent);
+            // If dropped outside, return to original
+            rectTransform.SetParent(originalParent);
         }
 
-        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-    }
-     public void OnPointerClick(PointerEventData eventData)
-    {
-        if (eventData.button == PointerEventData.InputButton.Left)
-        {
-            // EquipmentManager.instance.Equip(this);
-        }
+        rectTransform.anchoredPosition = Vector2.zero;
     }
 }
+
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.EventSystems;
+
+// public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+// {
+//     Transform originalParent;
+//     CanvasGroup canvas;
+
+
+
+//     private void Awake()
+//     {
+//         canvas = GetComponentInParent<CanvasGroup>();
+//     }
+
+//     public void OnBeginDrag(PointerEventData eventData)
+//     {
+//         originalParent = transform.parent;
+//         transform.SetParent(transform.root);
+//         canvas.blocksRaycasts = false;
+//         canvas.alpha = 0.6f;
+//     }
+
+//     public void OnDrag(PointerEventData eventData)
+//     {
+//         transform.position = eventData.position;
+//     }
+
+//     public void OnEndDrag(PointerEventData eventData)
+//     {
+//         canvas.blocksRaycasts = true;
+//         canvas.alpha = 1f;
+
+//         Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+//         if (dropSlot == null)
+//         {
+//             GameObject item = eventData.pointerEnter;
+//             if (item !=null)
+//             {
+//                 dropSlot = item.GetComponentInParent<Slot>();
+//             }
+//         }
+//         Slot originalSlot = originalParent?.GetComponent<Slot>();
+
+//         if (dropSlot != null)
+//         {
+//             if (dropSlot.currentItem != null)
+//             {
+//                 dropSlot.currentItem.transform.SetParent(originalSlot.transform);
+//                 originalSlot.currentItem = dropSlot.currentItem;
+//                 dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+//             }
+//             else
+//             {
+//                 originalSlot.currentItem = null;
+//             }
+
+//             transform.SetParent(dropSlot.transform);
+//             dropSlot.currentItem = gameObject;
+//         }
+//         else
+//         {
+//             transform.SetParent(originalParent);
+//         }
+
+//         GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+//     }
+//      public void OnPointerClick(PointerEventData eventData)
+//     {
+//         if (eventData.button == PointerEventData.InputButton.Left)
+//         {
+//             // EquipmentManager.instance.Equip(this);
+//         }
+//     }
+// }
